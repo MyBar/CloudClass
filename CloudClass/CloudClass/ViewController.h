f018d1e6dc5ac07924ae582d02e0b5959231ffdc
“MyBar” <450438946@qq.com>
2015-09-25 21:12:21 +0800

    create CloudClass project

---
+++ CloudClass/CloudClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudClass
+//
+//  Created by lijingui2010 on 15/9/25.
+//  Copyright © 2015年 MyBar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CloudClass/CloudClass/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CloudClass
+//
+//  Created by lijingui2010 on 15/9/25.
+//  Copyright © 2015年 MyBar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
